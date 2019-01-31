4841b56d6ba534e2d9a54f5b9ea48fc99767fe34
Lobzin Roman <lobzinra@24au.ru>
2019-01-31 22:04:57 +0700

    Небольшой рефакторинг

--- main.go
+++ main.go
@@ -6,34 +6,35 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"time"
 )
 
 const (
-	ApiToken = "yourApiTokenHere"
-	ApiUrl   = "https://api.telegram.org/bot%s/%s"
+	apiURL = "https://api.telegram.org/bot%s/%s"
 )
 
 var (
-	httpClient http.Client
+	apiToken   string
+	httpClient *http.Client
+	offset     int
 )
 
+func init() {
+	httpClient = &http.Client{
+		Timeout: 10 * time.Second,
+	}
+}
+
 func main() {
-	defer func() {
-		r := recover()
-		if r != nil {
-			fmt.Println(r)
-		}
-	}()
+	if len(os.Args) < 2 {
+		panic(fmt.Errorf("ApiToken not found"))
+	}
 
-	httpClient = http.Client{}
+	apiToken = os.Args[1]
 
-	getMe()
-
-	offset := 0
-
-	ch := getUpdatesChan(offset)
+	ch := getUpdatesChan()
 
 	for update := range ch {
 		proccess(update)
@@ -82,27 +83,25 @@ func getUpdates(offset int) ([]Update, error) {
 	return updates, nil
 }
 
-func getUpdatesChan(offset int) chan Update {
+func getUpdatesChan() chan Update {
 	ch := make(chan Update)
-
 	go func() {
 		for {
 			updates, err := getUpdates(offset)
 			if err != nil {
-				fmt.Printf("ERROR: %v\n", err)
+				logError(err)
 				time.Sleep(time.Second * 5)
 				continue
 			}
 
 			for _, update := range updates {
-				offset = update.Id + 1
+				offset = update.ID + 1
 				ch <- update
 			}
 
 			time.Sleep(time.Second * 1)
 		}
 	}()
-
 	return ch
 }
 
@@ -110,7 +109,7 @@ func sendMessage(msg MessageData) (bool, error) {
 	const methodName = "sendMessage"
 
 	data := url.Values{}
-	data.Add("chat_id", strconv.Itoa(msg.ChatId))
+	data.Add("chat_id", strconv.Itoa(msg.ChatID))
 	data.Add("text", msg.Text)
 	data.Add("parse_mode", msg.ParseMode)
 	data.Add("reply_to_message_id", strconv.Itoa(msg.ReplyTo))
@@ -125,27 +124,29 @@ func sendMessage(msg MessageData) (bool, error) {
 
 func proccess(update Update) {
 	msg := MessageData{
-		ChatId:    update.Message.Chat.Id,
+		ChatID:    update.Message.Chat.ID,
 		Text:      fmt.Sprintf("Hi, %s! I'm bot", update.Message.From.FirstName),
 		ParseMode: "HTML",
-		ReplyTo:   update.Message.Id,
+		ReplyTo:   update.Message.ID,
 	}
 
 	_, err := sendMessage(msg)
 	if err != nil {
-		fmt.Printf("ERROR: %v", err)
+		logError(err)
 	}
 }
 
 func sendRequest(methodName string, data url.Values) (ApiResponse, error) {
 	r := ApiResponse{}
 
-	url := fmt.Sprintf(ApiUrl, ApiToken, methodName)
+	url := fmt.Sprintf(apiURL, apiToken, methodName)
 
 	resp, err := httpClient.PostForm(url, data)
 	if err != nil {
 		return r, err
 	}
+
+	defer resp.Body.Close()
 
 	bytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -160,6 +161,10 @@ func sendRequest(methodName string, data url.Values) (ApiResponse, error) {
 	return r, nil
 }
 
+func logError(err error) {
+	fmt.Printf("ERROR: %v\n", err)
+}
+
 type ApiResponse struct {
 	Ok          bool
 	Description string
@@ -167,28 +172,28 @@ type ApiResponse struct {
 }
 
 type Update struct {
-	Id      int `json:"update_id"`
+	ID      int `json:"update_id"`
 	Message Message
 }
 
 type Message struct {
-	Id   int `json:"message_id"`
+	ID   int `json:"message_id"`
 	From User
 	Chat Chat
 	Text string
 }
 
 type User struct {
-	Id        int
+	ID        int
 	FirstName string `json:"first_name"`
 }
 
 type Chat struct {
-	Id int
+	ID int
 }
 
 type MessageData struct {
-	ChatId    int
+	ChatID    int
 	Text      string
 	ParseMode string
 	ReplyTo   int
